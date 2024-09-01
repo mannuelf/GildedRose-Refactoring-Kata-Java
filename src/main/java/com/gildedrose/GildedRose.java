a944b35ec9f2d9dfdc30a553397017027de6c242
Mannuel Ferreira <mannuel.ferreira@storebrand.no>
2024-09-01 13:35:50 +0200

    refactor: extract function, isAgedBrie

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -7,10 +7,14 @@ class GildedRose {
         this.items = items;
     }
 
+    public boolean isAgedBrie(String itemName) {
+        if(itemName.length() == 0) return false;
+        return itemName.contains("Aged Brie");
+    }
+
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (isAgedBrie(items[i].name) && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,12 +6,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+    Item[] items = new Item[] {
+        new Item("+5 Dexterity Vest", 10, 20), //
+        new Item("Aged Brie", 2, 0), //
+        new Item("Elixir of the Mongoose", 5, 7), //
+        new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+        new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        // this conjured item does not work properly yet
+        new Item("Conjured Mana Cake", 3, 6) };
+
+    GildedRose app = new GildedRose(items);
+
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
+    void item() {
+        String expectedItemName = "Conjured Mana Cake";
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        assertEquals(expectedItemName, app.items[8].name);
+    }
+
+    @Test
+    void isAgedBrie() {
+        String expectedItemName = "Aged Brie";
+        assertEquals(expectedItemName, app.items[1].name);
     }
 
 }
